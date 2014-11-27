93f0a04a307bc4e718ff1c3c9a4e303c49105af7
iuliiabaranovska <iuliia.baranovska@gmail.com>
2014-11-27 21:59:11 +0200

    index page added

---
+++ lvivuniversitiesmap/lvivuniversitiesmap/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace lvivuniversitiesmap.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
